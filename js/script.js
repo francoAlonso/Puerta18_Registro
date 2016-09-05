506bff96e7a24b48ca8f6d94de82ffd88f4f2447
francoAlonso <fra.n.co@hotmail.com>
2016-09-04 22:29:49 -0300

    Check In done
    una animacion beta del checkIn

--- js/script.js
+++ js/script.js
@@ -4,29 +4,33 @@ $(document).ready(function() {
   var nombreIngresado = document.getElementById("input");
   var statusIcon = document.getElementById("statusIcon");
 
-  //$(statusIcon).hide();
+  $(statusIcon).hide();
  
   $( nombreIngresado ).autocomplete({
+
     minLength: 4,//caracteres minimos para buscar
     delay: 500,//cuanto tarda en empezar a buscar dps de tipear
+    source: "http://localhost/ejercicio/prueba/php/index.php/buscarUsuarios?term=" + nombreIngresado.value,
+    position: {
+        my: "left+0 top+8",
+    },
     search: function(){
       selectStatusIcon("loading");
     },
-    source: "http://localhost/ejercicio/prueba/php/index.php/buscarUsuarios?term=" + 
-      nombreIngresado.value,
     focus: function( event, ui ) { //cuando paso por las respuestas
       $( nombreIngresado ).val( ui.item.Nombre_Completo );
       return false;
     },
     response: function(event, ui) {
       // ui.content is the array that's about to be sent to the response callback.
-      if (ui.content.length === 0) {
+      if (!ui.content.length) {
         informStatus("no existe", "D91967");
       }
-        $(statusIcon).hide();
+      $(statusIcon).hide();
     },
     select: function( event, ui ) { //cuando selecciono la opcion
       var nombre = ui.item.Nombre_Completo;
+      selectStatusIcon("loading");
       $.ajax({
         type: "GET",  
         url: "http://localhost/ejercicio/prueba/php/index.php/login/" + nombre,
@@ -43,26 +47,33 @@ $(document).ready(function() {
   }).autocomplete( "instance" )._renderItem = function( ul, item ) {
     var estado = "";
     if(item.Conectado == 0){
-      estado = "<a class='entrar'>entrar</a>";
+      estado = "<a class='entrar'>Entrar</a>";
     }else{
-      estado = "<a class='salir'>salir</a>";
+      estado = "<a class='salir'>Salir</a>";
     }
     return $( "<li>" )
       .append("<div class='theDiv'>" + item.Nombre_Completo + estado + "</div>" )
       .appendTo( ul );
   };
 
+  var timeOut; //variable global para selectStatusIcon() e informStatus()
 
   function selectStatusIcon(estado){
+    clearTimeout(timeOut);
     switch(estado) {
-      case "checkIn":
-        statusIcon.src = ("img/" + estado + ".png"); 
-        $(statusIcon).show(500);
-        $(statusIcon).hide(500);
+      case "checkIn": 
+        statusIcon.src = ("img/" + estado + ".gif"); 
+        $(statusIcon).show();
+        timeOut = setInterval(function(){
+          $(statusIcon).hide();
+        }, 500);
         break;
       case "cross":
         statusIcon.src = ("img/" + estado + ".png"); 
         $(statusIcon).show();
+        timeOut = setInterval(function(){
+          $(statusIcon).hide();
+        }, 500);
         break;
       case "loading":
         statusIcon.src = ("img/" + estado + ".gif"); 
@@ -71,7 +82,6 @@ $(document).ready(function() {
     }
   }//selectStatusIcon
 
-  var timeOut;
   function informStatus(estado, color){
     clearTimeout(timeOut);
     statusInfo.innerHTML = estado;
